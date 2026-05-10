c8fb3fc6e13973bcb6fa741a0a8f0d652e9c11d7
ronnelcuevas143-design <ronnelcuevas143@gmail.com>
2026-05-10 09:41:05 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CRT Tracker</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.jsx"></script>
-  </body>
-</html>
